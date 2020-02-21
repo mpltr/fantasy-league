67b6e6c928af572e127ad998f666fdec8275ad1c
sectarian <matthew.poulter0@gmail.com>
2020-02-21 22:09:58 +0000

    table styling done

--- pages/tournaments/[id].js
+++ pages/tournaments/[id].js
@@ -8,36 +8,41 @@ const Tournament = (props) => {
     const tournamentId = props.url.query.id;
 
     return ( 
-        <div>
-            <h1>Tournament {tournamentId} Page</h1>
+        <div className="container">
+            <h1>{props.name}</h1>
+            <Tabs>
+                <div tabtitle="Groups">
+                    
                     <Tabs>
-                        <div tabtitle="Groups">
-                           
-                            <Tabs>
-                                    {props.tables && Object.keys(props.tables).map((key, i) => {
-                                        return (
-                                            <div tabtitle={`Group  ${key}`} >
-                                    
-                                                <Tabs>
-                                                    <Table tabtitle="Table"
-                                                           players={props.players}
-                                                           tablePlayerIds={props.tables[key]}>
-                                                    </Table>
-                                                    <Fixtures tabtitle="Fixtures"
-                                                              fixtures={props.fixtures[key]}
-                                                              players={props.players}>
-                                                        Fixtures
-                                                    </Fixtures>
-                                                </Tabs>
-                                            </div>
-                                        );
-                                    })}
-                            </Tabs>
-                        </div>
-                        <div tabtitle="Knockout">Knockout</div>
-                        <div tabtitle="Stats">Stats</div>
+                            {props.tables && Object.keys(props.tables).map((key, i) => {
+                                return (
+                                    <div tabtitle={`Group  ${key}`} >
+                            
+                                        <Tabs>
+                                            <Table tabtitle="Table"
+                                                    players={props.players}
+                                                    tablePlayerIds={props.tables[key]}>
+                                            </Table>
+                                            <Fixtures tabtitle="Fixtures"
+                                                      fixtures={props.fixtures[key]}
+                                                      players={props.players}>
+                                                Fixtures
+                                            </Fixtures>
+                                        </Tabs>
+                                    </div>
+                                );
+                            })}
                     </Tabs>
-
+                </div>
+                <div tabtitle="Knockout">Knockout</div>
+                <div tabtitle="Stats">Stats</div>
+            </Tabs>
+            <style jsx>{`
+                .container {
+                    max-width: 800px;
+                    margin: auto;
+                }
+            `}</style>
         </div>
     )
 }
@@ -48,6 +53,7 @@ Tournament.getInitialProps = async (context) => {
     const data = await fetch(`http://localhost/fantasy-league-api/public/get-tournament/${uid}`).then(res => res.json());
 
     return {
+        name: data.name, 
         players: data.players,
         tables: data.tables,
         fixtures: data.fixtures
