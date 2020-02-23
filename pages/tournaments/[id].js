ff1d1a2c19f4e7ae18f538b52aea100364b62db4
Matt Poulter <matt.poulter@apsleygroup.co.uk>
2020-02-23 22:09:53 +0000

    tabs allowed a single element now

--- pages/tournaments/[id].js
+++ pages/tournaments/[id].js
@@ -10,29 +10,44 @@ const Tournament = (props) => {
             <h1>{props.name}</h1>
             <Tabs>
                 <div tabtitle="Groups">
-                    
                     <Tabs>
-                            {props.tables && Object.keys(props.tables).map((key, i) => {
-                                return (
-                                    <div key={i} tabtitle={`Group  ${key}`} >
-                                        <Tabs>
-                                            <Table tabtitle="Table"
-                                                    players={props.players}
-                                                    tablePlayerIds={props.tables[key]}>
-                                            </Table>
-                                            <Fixtures tabtitle="Fixtures"
-                                                      fixtures={props.fixtures[key]}
-                                                      players={props.players}>
-                                                Fixtures
-                                            </Fixtures>
-                                        </Tabs>
-                                    </div>
-                                );
-                            })}
+                        {props.tables && Object.keys(props.tables).map((key, i) => {
+                            return (
+                                <div key={i} tabtitle={`Group  ${key}`} >
+                                    <Tabs>
+                                        <Table tabtitle="Table"
+                                                players={props.players}
+                                                tablePlayerIds={props.tables[key]}
+                                                numberOfGroupTeamsToProgress={props.numberOfGroupTeamsToProgress}>
+                                        </Table>
+                                        <Fixtures tabtitle="Fixtures"
+                                                    fixtures={props.fixtures[key]}
+                                                    players={props.players}>
+                                            Fixtures
+                                        </Fixtures>
+                                    </Tabs>
+                                </div>
+                            );
+                        })}
                     </Tabs>
                 </div>
-                <div tabtitle="Knockout">Knockout</div>
-                <div tabtitle="Stats">Stats</div>
+                { props.fixtures['Last 16'] &&
+                    <Tabs tabtitle="Knockouts" test={true} >
+                        <Fixtures tabtitle="Last 16"
+                                  fixtures={props.fixtures['Last 16']}
+                                  players={props.players}>
+                        </Fixtures>
+                        {/* <div></div> */}
+                    </Tabs>
+                }
+                <div tabtitle="Stats">
+                    <Tabs>
+                        <Table tabtitle="Overall Table"
+                               players={props.players}
+                               tablePlayerIds={Object.keys(props.players).map(playerId => playerId)}
+                        ></Table>
+                    </Tabs>
+                </div>
             </Tabs>
             <style jsx>{`
                 .container {
@@ -54,9 +69,11 @@ Tournament.getInitialProps = async (context) => {
 
     return {
         name: data.name, 
+        numberOfGroupTeamsToProgress: data.numberOfGroupTeamsToProgress, 
         players: data.players,
         tables: data.tables,
-        fixtures: data.fixtures
+        fixtures: data.fixtures,
+        knockout: data.knockout
     };
 }
  
