ea7d6df99442a03df23b226868aeb0f68144e4a3
Matt Poulter <matt.poulter@apsleygroup.co.uk>
2020-09-12 17:02:06 +0100

    table widths made consistent and ellipsis added for long names on fixtures

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import './styles.css'
+import Head from "next/head"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"></link>
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
